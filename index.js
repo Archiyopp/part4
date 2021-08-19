4c25197065535ee78130104e3bae234fc9b1b026
Cristian Andres Fernandez Bermudez <crafernandezbe@unal.edu.co>
2021-08-19 10:01:10 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const blogsRouter = require('./controllers/blogs');
+const morgan = require('morgan');
+const config = require('./utils/config');
+mongoose.connect(config.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/blogs', blogsRouter);
+
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`);
+});
